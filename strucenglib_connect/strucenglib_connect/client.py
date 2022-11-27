67b86782c5bd7d549a8b2aa9e0e26ccf440a9b0e
Andrin Bertschi <hi@abertschi.ch>
2022-11-27 19:18:13 +0100

    wip

--- strucenglib_connect/strucenglib_connect/client.py
+++ strucenglib_connect/strucenglib_connect/client.py
@@ -11,8 +11,6 @@ logger = logging.getLogger(__name__)
 
 
 def do_analyse_and_extract(server, data):
-    # global do_print
-    # do_print = print_callback
     c = Client(server)
     return c.execute('analyse_and_extract', data)
 
--- strucenglib_connect/strucenglib_connect/wrapper.py
+++ strucenglib_connect/strucenglib_connect/wrapper.py
@@ -1,8 +1,7 @@
 from strucenglib_connect.client import Client
 
+# RPC wrapper to use with compas.rpc
 def do_analyse_and_extract(server, data):
-    # global do_print
-    # do_print = print_callback
     c = Client(server)
     return c.execute('analyse_and_extract', data)
 

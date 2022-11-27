623f3afa4e306ce879699d78547294c7419d4c5c
Andrin Bertschi <hi@abertschi.ch>
2022-11-27 17:40:10 +0100

    fix import issues

--- strucenglib_connect/src/strucenglib_connect/strucenglib_connect.py
+++ strucenglib_connect/src/strucenglib_connect/strucenglib_connect.py
@@ -1,30 +1,30 @@
 from compas_fea.structure import Structure
 
-from marshall import json_to_obj, obj_to_json
+# from marshall import json_to_obj, obj_to_json
 
 from compas.rpc import Proxy
 
 def analyse_and_extract(server, structure, **kwargs):
-    exec = {
-        'args': kwargs,
-        'structure': obj_to_json(structure)
-    }
-    try:
-        client = Proxy('client')
-    except:
-        client = Proxy('strucenglib_connect.client')
-
-    response = client.do_analyse_and_extract(server, exec)
-    if not response:
-        print('response is null, error')
-        return None
-
-    print(response.get('stdout'))
-    success = response.get('success')
-    payload = response.get('payload')
-    if success and payload is not None:
-        return json_to_obj(response['payload'])
-
+    # exec = {
+    #     'args': kwargs,
+    #     'structure': obj_to_json(structure)
+    # }
+    # try:
+    #     client = Proxy('client')
+    # except:
+    #     client = Proxy('strucenglib_connect.client')
+    #
+    # response = client.do_analyse_and_extract(server, exec)
+    # if not response:
+    #     print('response is null, error')
+    #     return None
+    #
+    # print(response.get('stdout'))
+    # success = response.get('success')
+    # payload = response.get('payload')
+    # if success and payload is not None:
+    #     return json_to_obj(response['payload'])
+    #
     return None
 
 

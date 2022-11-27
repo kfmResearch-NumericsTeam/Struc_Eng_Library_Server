4d7d831ec2095715cb9a22f6ef9bfd664329f79d
Andrin Bertschi <hi@abertschi.ch>
2022-11-27 19:14:23 +0100

    wip

--- strucenglib_connect/strucenglib_connect/connect.py
+++ strucenglib_connect/strucenglib_connect/connect.py
@@ -1,3 +1,5 @@
+
+
 from strucenglib_connect.marshall import json_to_obj, obj_to_json
 from compas.rpc import Proxy
 
@@ -11,26 +13,28 @@ def analyse_and_extract(server, structure, **kwargs):
         client = Proxy('strucenglib_connect.wrapper')
     except Exception as e:
         print(e)
+        raise (e)
 
 
     print('b')
     response = client.do_analyse_and_extract(server, data)
     print('c')
     print(response)
-    if not response:
-        print('response is null, error')
-        return None
-
-    print(response.get('stdout'))
-    success = response.get('success')
-    payload = response.get('payload')
-    if success and payload is not None:
-        return json_to_obj(response['payload'])
+    # if not response:
+    #     print('response is null, error')
+    #     return None
+    #
+    # print(response.get('stdout'))
+    # success = response.get('success')
+    # payload = response.get('payload')
+    # if success and payload is not None:
+    #     return json_to_obj(response['payload'])
 
     return None
 
 #
 # if __name__ == '__main__':
+#     from compas_fea.structure import Structure
 #     s = Structure('/tmp/', 'test')
 #     res = analyse_and_extract('ws://localhost:8007', s, software='abaqus')
 #     print(res)

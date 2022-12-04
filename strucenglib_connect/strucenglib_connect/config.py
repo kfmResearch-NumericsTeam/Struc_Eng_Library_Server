5470eaffccd36e07509a5ce18944e702136a388f
Andrin Bertschi <hi@abertschi.ch>
2022-12-04 18:43:32 +0100

    enable proxy

--- strucenglib_connect/strucenglib_connect/config.py
+++ strucenglib_connect/strucenglib_connect/config.py
@@ -1,7 +1,7 @@
 from strucenglib_connect.serialize_json import set_whitelist
 from strucenglib_connect.whitelist import FEA_WHITE_LIST
 
-WITH_PROXY = False
+WITH_PROXY = True
 set_whitelist(FEA_WHITE_LIST)
 SERIALIZE_CLIENT_TO_SERVER = 'json'
 SERIALIZE_SERVER_TO_CLIENT = 'pickle'

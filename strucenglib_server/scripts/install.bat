3e6bee368fd2c3b16a695273d81600021fdaabc6
Andrin Bertschi <hi@abertschi.ch>
2022-12-07 16:37:28 +0100

    update readme

--- strucenglib_server/scripts/install.bat
+++ strucenglib_server/scripts/install.bat
@@ -4,6 +4,6 @@ set cenv=strucenglib_connect
 
 call conda activate %cenv%
 call python -m compas_rhino.install -v 7.0
-call pip install https://github.com/kfmResearch-NumericsTeam/Struc_Eng_Library_Server/archive/feature/client-server.zip#subdirectory=strucenglib_connect
+call pip install https://github.com/kfmResearch-NumericsTeam/Struc_Eng_Library_Server/archive/feature/master.zip#subdirectory=strucenglib_connect
 call python -m compas_rhino.install -v 7.0 -p strucenglib_connect
 pause
